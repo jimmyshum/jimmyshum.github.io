c2121daf2e2063eaf8ae28b325403db415826dca
jimmyshum <jimmyshum2001@gmail.com>
2014-07-15 13:37:18 -0500

    website optimization 1-11

--- blog.js
+++ blog.js
@@ -1,12 +1,15 @@
-function readCatalog(file){
+function readCatalog(file,onComplete,onError){
 	var catFile = new XMLHttpRequest();
 	catFile.open("GET",file, true);
 	catFile.onreadystatechange = function() {
 		if (catFile.readyState === 4){   // Makes sure the document is ready to parse.
     		if (catFile.status === 200 || catFile.status == 0) {  // Makes sure it's found the file.
 				var catText = catFile.responseText;
-				return catText;
+				onComplete(catText);
+				
 			}
+			else
+				onError();
 	
 		}
 		
@@ -15,7 +18,7 @@ function readCatalog(file){
 
 }
 
-function readBlogTxt(file){
+function readBlogTxt(file,onComplete,onError){
 	var txtFile = new XMLHttpRequest();
 	txtFile.open("GET",file, true);
 	txtFile.onreadystatechange = function() {
@@ -23,8 +26,10 @@ function readBlogTxt(file){
     		if (txtFile.status === 200 || txtFile.status == 0) {  // Makes sure it's found the file.
 				var allText = txtFile.responseText;
 				//alert(allText);
-				return allText;
+				onComplete(allText);
 			}
+			else
+				onError();
 	
 		}
 		
